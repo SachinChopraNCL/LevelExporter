b913f602c88a4b22030b3aad5fa6c9658937cd1f
Sachin Chopra <39964339+SachinChopraNCL@users.noreply.github.com>
2021-03-16 16:31:38 +0000

    All

---
+++ Assets/ComponentIdentifier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentIdentifier : MonoBehaviour
+{
+    public enum LevelComponentType 
+    {
+        Plane,
+        Obstacle,
+        Wall, 
+        Player
+    }
+    public LevelComponentType obstacleType;
+
+}
---
+++ Assets/LevelExporter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelExporter : MonoBehaviour
+{
+    GameObject[] allGameObject;
+    SaveData data = new SaveData();
+    void Start()
+    {
+        allGameObject =  GameObject.FindGameObjectsWithTag("LevelComponent") as GameObject[];
+        for(int i = 0; i < allGameObject.Length; i++)
+        {
+            LevelComponent levelComponent = new LevelComponent();
+            ComponentIdentifier identifier = allGameObject[i].GetComponent<ComponentIdentifier>();
+            levelComponent.objectName = identifier.obstacleType.ToString();
+            levelComponent.position = allGameObject[i].transform.position;
+            levelComponent.rotation = allGameObject[i].transform.rotation;
+            levelComponent.scale = allGameObject[i].transform.localScale;
+            data.levelComponentData.levelComponents.Add(levelComponent);
+        }
+        data.SaveIntoJson();
+  }
+
+}
+
+public class SaveData : MonoBehaviour
+{
+    public LevelComponentData levelComponentData = new LevelComponentData();
+
+    public void SaveIntoJson()
+    {
+        string levelComponent = JsonUtility.ToJson(levelComponentData, true);
+        Debug.Log(Application.persistentDataPath);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/" + SceneManager.GetActiveScene().name + ".json", levelComponent);
+
+    }
+}
+
+[System.Serializable]
+public class LevelComponentData
+{    
+    public List<LevelComponent> levelComponents = new List<LevelComponent>(); 
+
+}
+
+[System.Serializable]
+public class LevelComponent
+{
+    public string objectName;
+    public Vector3 position;
+    public Quaternion rotation;
+    public Vector3 scale; 
+}
